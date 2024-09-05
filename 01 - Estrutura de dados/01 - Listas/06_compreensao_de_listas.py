06735de1fe2365f148acc1e9e4f74ccbedbec668
Ray-King11 <raidugo.21@outlook.com>
2024-09-05 18:07:03 -0300

    feat: add module 6

--- 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
+++ 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
@@ -15,6 +15,19 @@ pares = [numero for numero in numeros if numero % 2 == 0]
 print(pares)
 
 # Modificar valores
+
+# Modificaçãod e valores versão 1
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = []
+
+for numero in numeros:
+    quadrado.append(numero **2)
+
+
+# Modificação de valores versão 2
 numeros = [1, 30, 21, 2, 9, 65, 34]
 quadrado = [numero**2 for numero in numeros]
 print(quadrado)
+
+numeros = [n**2 if n > 6 else n for n in range(10) if n % 2 == 0]
+print(numeros)
