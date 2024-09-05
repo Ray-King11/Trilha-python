227b56d50a0a27e9f4b3823393f26f6bae02e886
Ray-King11 <raidugo.21@outlook.com>
2024-09-05 15:53:57 -0300

    Modification

--- 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
+++ 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
@@ -1,4 +1,15 @@
 # Filtrar lista
+
+ # Filtro versão 1
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = []
+
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+       
+
+# Filtro versão 2
 numeros = [1, 30, 21, 2, 9, 65, 34]
 pares = [numero for numero in numeros if numero % 2 == 0]
 print(pares)
