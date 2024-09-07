2852418759c475ccad2f2f118e65a95e0e26a4e2
Ray-King11 <raidugo.21@outlook.com>
2024-09-07 12:43:05 -0300

    Modification

--- 04 - Data e hora/1_datetime.py
+++ 04 - Data e hora/1_datetime.py
@@ -4,10 +4,10 @@ data = date(2023, 7, 10)
 print(data)
 print(date.today())
 
+hora = time(10, 20, 0)
+print(hora)
 
 data_hora = datetime(2023, 7, 10)
 print(data_hora)
 print(datetime.today())
 
-hora = time(10, 20, 0)
-print(hora)
